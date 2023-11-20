633fc5e24a25d150a30cc5718aa58a5077fd1940
Tutku Uçan <tutku.ucan@carboninteraktif.com>
2023-11-20 17:42:53 +0300

    Updated spacers

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,26 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: 'production', // production modunda çalışacak şekilde ayarlandı
-  entry: path.resolve(__dirname, 'src/kuick.scss'), // SCSS dosyasının giriş noktası
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/kuick.scss'),
   output: {
-    path: path.resolve(__dirname, 'dist'), // çıktı dosyasının kaydedileceği dizin
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.scss$/, // SCSS dosyalarını işlemek için test
+        test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader, // CSS dosyalarını ayırmak için kullanılan loader
-          'css-loader', // CSS'yi yüklemek ve içeri aktarmak için kullanılan loader
-          'sass-loader', // SCSS dosyalarını CSS'e çevirmek için kullanılan loader
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
       },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'kucik.min.css', // Ayıklanan ve minify edilen CSS dosyasının adı
+      filename: 'kucik.min.css',
     }),
   ],
   optimization: {
